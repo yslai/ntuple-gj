cd79aaa9ef57e44a640bc4f5e63a2f58ce5f7e93
Yue Shi Lai <ylai@users.sourceforge.net>
2017-12-04 07:42:46 -0800

    Allow skim and stored cut variables to be configured

--- macros/AddAliAnalysisTaskNTGJ.C
+++ macros/AddAliAnalysisTaskNTGJ.C
@@ -1,63 +1,83 @@
 AliAnalysisTaskNTGJ *
-AddAliAnalysisTaskNTGJ(TString name = "AliAnalysisTaskNTGJ")
+AddAliAnalysisTaskNTGJ(TString name = "AliAnalysisTaskNTGJ",
+                       TString emcal_correction_filename =
+                       "emcal_correction.yaml",
+                       double skim_cluster_min_e = -1e+309,
+                       double skim_track_min_pt = -1e+309,
+                       double skim_muon_track_min_pt = -1e+309,
+                       double skim_jet_min_pt_1 = -1e+309,
+                       double skim_jet_min_pt_2 = -1e+309,
+                       double skim_jet_min_pt_3 = -1e+309,
+                       double skim_multiplicity_tracklet_min_n = -2147483648,
+                       double stored_track_min_pt = -1e+309,
+                       double stored_jet_min_pt_raw = -1e+309)
 {
-	AliAnalysisManager *mgr =
-		AliAnalysisManager::GetAnalysisManager();
-	AliAnalysisTaskNTGJ *task =
-		new AliAnalysisTaskNTGJ(name.Data());
+    AliAnalysisManager *mgr =
+        AliAnalysisManager::GetAnalysisManager();
+    AliAnalysisTaskNTGJ *task =
+        new AliAnalysisTaskNTGJ(name.Data());
 
-	// gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/"
-	// 				 "AddTaskCentrality.C");
+    // gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/"
+    //               "AddTaskCentrality.C");
 
-	// AliCentralitySelectionTask *taskCentrality = AddTaskCentrality();
-	// if (useMC) taskCentrality->SetMCInput();
+    // AliCentralitySelectionTask *taskCentrality = AddTaskCentrality();
+    // if (useMC) taskCentrality->SetMCInput();
 
-	gROOT->LoadMacro("$ALICE_PHYSICS/OADB/COMMON/MULTIPLICITY/"
-					 "macros/AddTaskMultSelection.C");
+    gROOT->LoadMacro("$ALICE_PHYSICS/OADB/COMMON/MULTIPLICITY/"
+                     "macros/AddTaskMultSelection.C");
 
-	AliMultSelectionTask *mult_selection_task = AddTaskMultSelection(kFALSE);
+    AliMultSelectionTask *mult_selection_task = AddTaskMultSelection(kFALSE);
 
-	gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/"
-					 "AddTaskEmcalCorrectionTask.C");
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/"
+                     "AddTaskEmcalCorrectionTask.C");
 
-	AliEmcalCorrectionTask *correction_task = AddTaskEmcalCorrectionTask();
+    AliEmcalCorrectionTask *correction_task = AddTaskEmcalCorrectionTask();
 
-	correction_task->
-		SetUserConfigurationFilename("emcal_correction.yaml");
-	correction_task->Initialize();
+    correction_task->SetUserConfigurationFilename(emcal_correction_filename.Data());
+    correction_task->Initialize();
 
-	AliEMCALRecoUtils *reco_util = task->GetEMCALRecoUtils();
+    AliEMCALRecoUtils *reco_util = task->GetEMCALRecoUtils();
   
-	gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/EMCAL/macros/"
-					 "ConfigureEMCALRecoUtils.C");
+    gROOT->LoadMacro("$ALICE_PHYSICS/PWGPP/EMCAL/macros/"
+                     "ConfigureEMCALRecoUtils.C");
   
-	ConfigureEMCALRecoUtils(reco_util, kFALSE, kTRUE, kTRUE,
-							kFALSE, kFALSE, kFALSE); 
+    ConfigureEMCALRecoUtils(reco_util, kFALSE, kTRUE, kTRUE,
+                            kFALSE, kFALSE, kFALSE); 
 
-	reco_util->SetNumberOfCellsFromEMCALBorder(0);
-	reco_util->SwitchOnRecalibration();
-	reco_util->SwitchOnRunDepCorrection();
+    reco_util->SetNumberOfCellsFromEMCALBorder(0);
+    reco_util->SwitchOnRecalibration();
+    reco_util->SwitchOnRunDepCorrection();
   
-	TString filename = mgr->GetCommonFileName();
+    TString filename = mgr->GetCommonFileName();
 
-	filename += ":AliAnalysisTaskNTGJ";
+    filename += ":AliAnalysisTaskNTGJ";
 
-	mgr->AddTask(task);
-	mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
-	mgr->ConnectOutput(task, 1,
-					   mgr->CreateContainer("tree", TTree::Class(),
-											AliAnalysisManager::
-											kOutputContainer,
-											filename.Data()));
+    mgr->AddTask(task);
+    mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
+    mgr->ConnectOutput(task, 1,
+                       mgr->CreateContainer("tree", TTree::Class(),
+                                            AliAnalysisManager::
+                                            kOutputContainer,
+                                            filename.Data()));
 
-	AliAnalysisAlien *plugin = mgr->GetGridHandler();
+    AliAnalysisAlien *plugin = mgr->GetGridHandler();
 
-	if (plugin != NULL) {
-		task->SetAliROOTVersion(plugin->GetAliROOTVersion());
-		task->SetAliPhysicsVersion(plugin->GetAliPhysicsVersion());
-		task->SetGridDataDir(plugin->GetGridDataDir());
-		task->SetGridDataPattern(plugin->GetDataPattern());
-	}
+    if (plugin != NULL) {
+        task->SetAliROOTVersion(plugin->GetAliROOTVersion());
+        task->SetAliPhysicsVersion(plugin->GetAliPhysicsVersion());
+        task->SetGridDataDir(plugin->GetGridDataDir());
+        task->SetGridDataPattern(plugin->GetDataPattern());
 
-	return task;
+        task->SetSkimClusterMinE(skim_cluster_min_e);
+        task->SetSkimTrackMinPt(skim_track_min_pt);
+        task->SetSkimMuonTrackMinPt(skim_muon_track_min_pt);
+        task->SetSkimJetMinPt(skim_jet_min_pt_1, skim_jet_min_pt_2,
+                              skim_jet_min_pt_3);
+        task->SetSkimMultiplicityTrackletMinN(
+            skim_multiplicity_tracklet_min_n);
+        task->SetStoredTrackMinPt(stored_track_min_pt);
+        task->SetStoredJetMinPtRaw(stored_jet_min_pt_raw);
+    }
+
+    return task;
 }
