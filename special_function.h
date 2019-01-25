a972ab0834d9a282b730bb28d354e025eddc9f31
Yue Shi Lai <ylai@users.sourceforge.net>
2019-01-25 14:33:52 -0800

    Split off EMCAL cell arithmetic from dependency on the ALICE class structure, fix the double precision angular range reduction (previously not 1 ulp precise in worst cases)

--- special_function.h
+++ special_function.h
@@ -7,6 +7,10 @@ namespace {
 
     void kahan_sum(double &s, double &s2, const double x)
     {
+        // The Kahan summation algorithm. See e.g. N. J. Higham, "The
+        // Accuracy of Floating Point Summation", SIAM J. Sci. Comput.
+        // 14(4), 783--799 (1993), https://doi.org/10.1137/0914050 .
+
         s2 += x;
 
         const double t = s + s2;
@@ -21,26 +25,47 @@ namespace {
             return x;
         }
 
-        static const double cody_waite_x_max = 1608.4954386379741381;
-        static const double two_pi_0 = 6.2831853071795649157;
-        static const double two_pi_1 = 2.1561211432631314669e-14;
-        static const double two_pi_2 = 1.1615423895917441336e-27;
+        // Cody and Waite range reduction with three constants. See
+        // e.g. J.-M. Muller, Elementary Functions: Algorithms and
+        // Implementation, 3rd Ed. (Springer, New York, 2016),
+        // https://doi.org/10.1007/978-1-4899-7983-4 , section 11.2.1,
+        // p. 203.
+
+        // The worst case for small IEEE 754 double precision
+        // arguments (that is until 6381956970095103 2^799, compare
+        // table 11.2, p. 211 in Muller 2016) is 3205513981387887
+        // 2^(-44) = 182.2..., which requires 59 additional digits for
+        // accurate reduction. And 53 - ceil(59 / 2) - 1 = 22,
+        // limiting the possible magnitude to 2^23 pi = 1.317... 10^7
+        static const double cody_waite_x_max_2 = ldexp(M_PI, 23);
+        static const double inverse_two_pi = 1 / (2 * M_PI);
         double ret;
 
-        if(x >= -cody_waite_x_max && x <= cody_waite_x_max) {
-            static const double inverse_two_pi =
-                0.15915494309189534197;
+        if (x >= -cody_waite_x_max_2 && x <= cody_waite_x_max_2) {
+            // 1686629713 2^(-28)
+            static const double two_pi_0 =  6.28318530693650245667;
+            // 560513589 2^(-61)
+            static const double two_pi_1 =  2.43084020360578856312e-10;
+            // 2 pi - 14488038916154245685 2^(-61)
+            static const double two_pi_2 = -1.00331152253366640471e-19;
             const double k = rint(x * inverse_two_pi);
-            ret = ((x - (k * two_pi_0)) - k * two_pi_1) -
-                k * two_pi_2;
+
+            ret = x - k * two_pi_0;
+            ret -= k * two_pi_1;
+            ret -= k * two_pi_2;
         }
         else {
+            // Fallback using the system trigonometric function
+
             long double sin_x;
             long double cos_x;
 
             sincosl(x, &sin_x, &cos_x);
             ret = (double)atan2l(sin_x, cos_x);
         }
+
+        // In all cases, avoid rounding to a duplicate -pi
+
         if(ret == -M_PI) {
             ret = M_PI;
         }
