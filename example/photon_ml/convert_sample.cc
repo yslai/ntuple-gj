6452d6b85b7d95dd14afde7a5faad7d18e6ab9ba
Yue Shi Lai <ylai@users.sourceforge.net>
2019-01-31 16:42:15 -0800

    Modification for full calorimeter 1d-convolution classification

--- example/photon_ml/convert_sample.cc
+++ example/photon_ml/convert_sample.cc
@@ -64,78 +64,6 @@ namespace {
         nphi = sm < 10 ? 24 : sm < 12 ? 8 : sm < 18 ? 24 : 8;
     }
 
-    bool inside_edge(unsigned int n, unsigned int d)
-    {
-        unsigned int sm;
-        unsigned int ieta;
-        unsigned int iphi;
-
-        to_sm_ieta_iphi(sm, ieta, iphi, n);
-
-        unsigned int neta;
-        unsigned int nphi;
-
-        neta_nphi(neta, nphi, sm);
-
-        return (ieta >= d && iphi >= d &&
-                ieta < neta - d && iphi < nphi - d);
-    }
-
-    void cell_5_5(unsigned int n_5_5[], const unsigned int n,
-                  const unsigned int ld = 5)
-    {
-        const unsigned int sm = n < 11520 ? n / 1152 :
-            n < 12288 ? 10 + (n - 11520) / 384 :
-            n < 16896 ? 12 + (n - 12288) / 768 :
-            18 + (n - 16896) / 384;
-        const unsigned int nphi =
-            sm < 10 ? 24 : sm < 12 ? 8 : sm < 18 ? 24 : 8;
-
-        n_5_5[0 * ld + 0] = n - 2 * nphi - 4;
-        n_5_5[0 * ld + 1] = n - 2 * nphi - 2;
-        n_5_5[0 * ld + 2] = n - 2 * nphi;
-        n_5_5[0 * ld + 3] = n - 2 * nphi + 2;
-        n_5_5[0 * ld + 4] = n - 2 * nphi + 4;
-        if (n % 2 == 0) {
-            n_5_5[1 * ld + 0] = n - 3;
-            n_5_5[1 * ld + 1] = n - 1;
-            n_5_5[1 * ld + 2] = n + 1;
-            n_5_5[1 * ld + 3] = n + 3;
-            n_5_5[1 * ld + 4] = n + 5;
-        }
-        else {
-            n_5_5[1 * ld + 0] = n - 2 * nphi - 5;
-            n_5_5[1 * ld + 1] = n - 2 * nphi - 3;
-            n_5_5[1 * ld + 2] = n - 2 * nphi - 1;
-            n_5_5[1 * ld + 3] = n - 2 * nphi + 1;
-            n_5_5[1 * ld + 4] = n - 2 * nphi + 3;
-        }
-        n_5_5[2 * ld + 0] = n - 4;
-        n_5_5[2 * ld + 1] = n - 2;
-        n_5_5[2 * ld + 2] = n;
-        n_5_5[2 * ld + 3] = n + 2;
-        n_5_5[2 * ld + 4] = n + 4;
-        if (n % 2 == 0) {
-            n_5_5[3 * ld + 0] = n + 2 * nphi - 3;
-            n_5_5[3 * ld + 1] = n + 2 * nphi - 1;
-            n_5_5[3 * ld + 2] = n + 2 * nphi + 1;
-            n_5_5[3 * ld + 3] = n + 2 * nphi + 3;
-            n_5_5[3 * ld + 4] = n + 2 * nphi + 5;
-        }
-        else {
-            n_5_5[3 * ld + 0] = n - 5;
-            n_5_5[3 * ld + 1] = n - 3;
-            n_5_5[3 * ld + 2] = n - 1;
-            n_5_5[3 * ld + 3] = n + 1;
-            n_5_5[3 * ld + 4] = n + 3;
-        }
-        n_5_5[4 * ld + 0] = n + 2 * nphi - 4;
-        n_5_5[4 * ld + 1] = n + 2 * nphi - 2;
-        n_5_5[4 * ld + 2] = n + 2 * nphi;
-        n_5_5[4 * ld + 3] = n + 2 * nphi + 2;
-        n_5_5[4 * ld + 4] = n + 2 * nphi + 4;
-    }
-
     void cell_neighbor(unsigned int n_m_m[], const unsigned int n,
                        const unsigned int m = 5, unsigned int ld = 0)
     {
@@ -167,65 +95,6 @@ namespace {
         }
     }
 
-    float unit_step(float x)
-    {
-        return std::isfinite(x) ? x >= 0.0F ? 1.0F : 0.0F : x;
-    }
-
-    void cluster_center_recal(float &pseudorapidity, float &azimuth)
-    {
-        static const float alice_emcal_granularity = 0.0143F;
-        const float p = pseudorapidity - alice_emcal_granularity * (
-	        6.26120e-03F -
-            7.93806e-02F * unit_step(azimuth - 1.745F) +
-            7.01157e-02F * unit_step(azimuth - 2.094F) -
-            5.67665e-02F * unit_step(azimuth - 2.443F) -
-            3.12072e-02F * unit_step(azimuth - 2.792F) -
-            0.0681282 + 0.30579 * pseudorapidity +
-            0.174691 * std::pow(pseudorapidity, 2));
-        const float a = azimuth - alice_emcal_granularity * (
-	        (1.80892e-01F + 3.88693e-02F + 2.17416e-02F + 9.48413e-03F) -
-            (2.88726e-01F + 7.01681e-02F + 1.31708e-02F - 7.20175e-04F) *
-            unit_step(azimuth - 1.745F) -
-            (1.77386e-01F + 8.19726e-02F + 4.11356e-02F + 1.04680e-03F) *
-            unit_step(azimuth - 2.094F) -
-            (1.38388e-01F + 3.72398e-02F + 2.81211e-03F + 7.69352e-03F) *
-            unit_step(azimuth - 2.443F) -
-            (1.02816e-01F + 3.30964e-02F - 8.73742e-03F + 6.21222e-03F) *
-            unit_step(azimuth - 2.792F) -
-            (0.00410491F + 0.000346243F) -
-            (0.0682701F + 0.0270903F) * pseudorapidity -
-            (0.060021F - 0.0362166F) * std::pow(pseudorapidity, 2));
-
-        pseudorapidity = p;
-        azimuth = a;
-    }
-
-    float min_sm_dazimuth(float pseudorapidity, float azimuth)
-    {
-        static const float sm_center[20] = {
-            1.57079583F, 1.57079670F, 1.91986285F, 1.91986059F,
-            2.26892674F, 2.26892899F, 2.61799219F, 2.61799514F,
-            2.96706059F, 2.96705799F, 3.20794364F, 3.20839677F,
-            -1.57079635F, -1.57079688F, -1.22173021F, -1.22173229F,
-            -0.87266589F, -0.87266172F, -0.63178698F, -0.63132969F
-        };
-        float min_dazimuth = M_PI;
-
-        for (size_t i = 0; i < 20; i++) {
-            if ((i % 2 == 0 ? 1.0F : -1.0F) * pseudorapidity >= 0) {
-                const float dazimuth =
-                    angular_range_reduce(azimuth - sm_center[i]);
-
-                if (fabsf(dazimuth) < min_dazimuth) {
-                    min_dazimuth = dazimuth;
-                }
-            }
-        }
-
-        return min_dazimuth;
-    }
-
 }
 
 #define NTRACK_MAX (1U << 15)
@@ -235,52 +104,17 @@ namespace {
 
 int main(int argc, char *argv[])
 {
-    unsigned int id[48][24];
-
-#if 1
-    for (unsigned int n = 0; ; n++) {
-        unsigned int sm;
-        unsigned int ieta;
-        unsigned int iphi;
-
-        to_sm_ieta_iphi(sm, ieta, iphi, n);
-
-        if (sm >= 1) {
-            break;
-        }
-        id[ieta][iphi] = n;
-    }
-    for (size_t i = 0; i < 10; i++) {
-        for (size_t j = 6; j < 9 + 6; j++) {
-            fprintf(stderr, "%3u ", id[i][j]);
-        }
-        fprintf(stderr, "\n");
-    }
-
-    for (unsigned int n = 117; n >= 116; n--) {
-        unsigned int n_9_9[7 * 7];
-
-        cell_neighbor(n_9_9, n, 7);
-        for (size_t i = 0; i < 7; i++) {
-            for (size_t j = 0; j < 7; j++) {
-                fprintf(stderr, "%3u ", n_9_9[i * 7 + j]);
-            }
-            fprintf(stderr, "< %3u\n", n);
-        }
-    }
-#endif
-
     if (argc < 3) {
         exit(EXIT_FAILURE);
     }
 
     // Access mode H5F_ACC_TRUNC truncates any existing file, while
     // not throwing any exception (unlike H5F_ACC_RDWR)
-    fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, argv[argc - 1]);
+    fprintf(stderr, "%s:%d: output HDF5 file `%s'\n", __FILE__, __LINE__, argv[argc - 1]);
     H5::H5File hdf5_file(argv[argc - 1], H5F_ACC_TRUNC);
     // How many properties per photon is written
-#define NCELL 5
-    static const size_t row_size_X = NCELL * NCELL + 4;
+#define NCELL 11
+    static const size_t row_size_X = NCELL * NCELL * 2 + 3;
     static const size_t row_size_y = 1;
     static const size_t row_size_lam = 1;
     // The tensor dimension increment for each new event
@@ -373,29 +207,31 @@ int main(int argc, char *argv[])
         TFile *root_file = TFile::Open(argv[iarg]);
 
         if (root_file == NULL) {
+            fprintf(stderr, "%s:%d: error: unable to open file "
+                    "`%s'\n", __FILE__, __LINE__, argv[iarg]);
             continue;
         }
 
         TDirectoryFile *df = dynamic_cast<TDirectoryFile *>
             (root_file->Get("AliAnalysisTaskNTGJ"));
+        TTree *_tree_event;
 
         if (df == NULL) {
-            continue;
+            _tree_event =
+                dynamic_cast<TTree *>(root_file->Get("_tree_event"));
+        }
+        else {
+            _tree_event =
+                dynamic_cast<TTree *>(df->Get("_tree_event"));
         }
 
-        TTree *_tree_event = dynamic_cast<TTree *>
-            (df->Get("_tree_event"));
-
         if (_tree_event == NULL) {
+            fprintf(stderr, "%s:%d:\n", __FILE__, __LINE__);
             continue;
         }
 
         fprintf(stderr, "%s:%d: %s (%d / %d)\n", __FILE__, __LINE__,
                 argv[iarg], iarg - 1, argc - 2);
-
-        Float_t multiplicity_v0[64];
-
-        _tree_event->SetBranchAddress("multiplicity_v0", multiplicity_v0);
 
         UInt_t ncluster;
         Float_t cluster_e[NCLUSTER_MAX];
@@ -410,7 +246,8 @@ int main(int argc, char *argv[])
         Float_t cluster_e_cross[NCLUSTER_MAX];
         Float_t cluster_s_nphoton[NCLUSTER_MAX][4];
         UInt_t cluster_nmc_truth[NCLUSTER_MAX];
-        UShort_t cluster_mc_truth_index[NCLUSTER_MAX][CLUSTER_NMC_TRUTH_MAX];
+        UShort_t cluster_mc_truth_index
+            [NCLUSTER_MAX][CLUSTER_NMC_TRUTH_MAX];
 
         _tree_event->SetBranchAddress("ncluster", &ncluster);
         _tree_event->SetBranchAddress("cluster_e", cluster_e);
@@ -456,9 +293,12 @@ int main(int argc, char *argv[])
 
         Float_t cell_e[17664];
         Float_t cell_tof[17664];
+        UShort_t cell_cluster_index[17664];
 
         _tree_event->SetBranchAddress("cell_e", cell_e);
         _tree_event->SetBranchAddress("cell_tof", cell_tof);
+        _tree_event->SetBranchAddress("cell_cluster_index",
+                                      cell_cluster_index);
 
         UInt_t nmc_truth;
         Float_t mc_truth_e[NMC_TRUTH_MAX];
@@ -474,9 +314,7 @@ int main(int argc, char *argv[])
         for (Long64_t i = 0; i < _tree_event->GetEntries(); i++) {
             _tree_event->GetEntry(i);
             for (UInt_t j = 0; j < ncluster; j++) {
-                if (cluster_e[j] >= 8.0F &&
-                    inside_edge(cluster_cell_id_max[j],
-                                (NCELL - 1) / 2) &&
+                if (cluster_e[j] >= 0.0F &&
                     // EMCAL noise suppression cut (with no particular
                     // name by the ALICE experiment)
                     cluster_ncell[j] >= 2 &&
@@ -487,6 +325,18 @@ int main(int argc, char *argv[])
 
                     cell_neighbor(neighbor, cluster_cell_id_max[j],
                                   NCELL);
+
+                    unsigned int sm;
+                    unsigned int ieta;
+                    unsigned int iphi;
+
+                    to_sm_ieta_iphi(sm, ieta, iphi,
+                                    cluster_cell_id_max[j]);
+
+                    unsigned int neta;
+                    unsigned int nphi;
+
+                    neta_nphi(neta, nphi, sm);
 
                     // Loop over all MC (ground) truth particles and
                     // decide if the particle is prompt by the highest
@@ -528,20 +378,33 @@ int main(int argc, char *argv[])
                         std::vector<unsigned int> y;
 
                         for (size_t k = 0; k < NCELL * NCELL; k++) {
-                            X.push_back(
-                                std::isfinite(cell_e[neighbor[k]]) ?
-                                cell_e[neighbor[k]] / cluster_e[j] : 0);
+                            int deta = k / NCELL - (NCELL - 1) / 2;
+                            int dphi = k % NCELL - (NCELL - 1) / 2;
+
+                            if (ieta + deta < neta &&
+                                iphi + dphi < nphi) {
+                                X.push_back(
+                                    std::isfinite(
+                                        cell_e[neighbor[k]]) ?
+                                    cell_e[neighbor[k]] /
+                                    cluster_e[j] :
+                                    0);
+                                X.push_back(
+                                    cell_cluster_index[neighbor[k]] ==
+                                    j);
+                            }
+                            else {
+                                X.push_back(0);
+                                X.push_back(0);
+                            }
                         }
                         X.push_back(1.0F / sqrtf(cluster_e[j]));
                         X.push_back(cluster_eta[j]);
-                        X.push_back(min_sm_dazimuth(
-                             cluster_eta[j], cluster_phi[j]));
-                        X.push_back(std::accumulate(
-                            multiplicity_v0,
-                            multiplicity_v0 + 64, 0));
+                        X.push_back(cluster_phi[j]);
                         y.push_back(prompt ? 1 : 0);
 
-                        std::vector<float> lam(1, cluster_lambda_square[j][0]);
+                        std::vector<float>
+                            lam(1, cluster_lambda_square[j][0]);
 
                         if (offset[0] == 0) {
                             data_set_X.write(&X[0], H5::PredType::
